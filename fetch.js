343f5c72ef33288c8103d317cb01ae88674e5eba
luckysaputra17 <damnlux17@gmail.com>
2022-11-02 23:37:39 +0700

    commit

--- fetch.js
+++ fetch.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch');
 const getindexApi  = () =>{
-    fetch('https://api-frontend.kemdikbud.go.id/hit_mhs/200511086',{
+    fetch('#',{
         method: 'GET'
     })
     .then(res => res.json())
@@ -8,4 +8,23 @@ const getindexApi  = () =>{
         console.log(result)
     })
 }
+// const postApi  = () =>{
+//     const data ={
+//         no: '1',
+//         catalog: 'test',
+//         notes: 'test'
+//     }
+//     fetch('https://api-frontend.kemdikbud.go.id/hit_mhs/200511086',{
+//         method: 'POST',
+//         headers: {
+//             'Content-Type: application/json'
+//         },
+//         body: JSON.stringify(data)
+//     })
+//     .then(res => res.json())
+//     .then(result => {
+//         console.log(result)
+//     })
+// }
+
 getindexApi()
